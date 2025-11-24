dbba3a7a56c94daa7810cd464daf9190f52612a8
MarcusBrustad <brustadmarcus@gmail.com>
2025-11-24 20:16:39 +0100

    Updated Repos to work with global exceptions rather than null values

--- src/TodoApi/Repositories/BaseRepository.cs
+++ src/TodoApi/Repositories/BaseRepository.cs
@@ -1,4 +1,5 @@
 ﻿using TodoApi.Data;
+using TodoApi.Exceptions;
 
 namespace TodoApi.Repositories;
 
@@ -7,31 +8,34 @@ public class BaseRepository<T>(TodoApiDbContext dbContext) : IBaseRepository<T>
 {
     protected readonly TodoApiDbContext _context = dbContext;
         
-    public async Task<T?> AddAsync(T entity)
+    public async Task<T> AddAsync(T entity)
     {
         await _context.Set<T>().AddAsync(entity);
         await _context.SaveChangesAsync();
         return entity;
     }
 
-    public async Task<T?> UpdateAsync(T entity)
+    public async Task<T> UpdateAsync(T entity)
     {
         _context.Set<T>().Update(entity);
         await _context.SaveChangesAsync();
         return entity;
     }
 
-    public async Task<T?> GetByIdAsync(Guid id)
+    public async Task<T> GetByIdAsync(Guid id)
     {
-        return await _context.Set<T>().FindAsync(id);
+        var entity = await _context.Set<T>().FindAsync(id);
+        if (entity == null)
+            throw new NotFoundException(typeof(T).Name, id);
+        return entity;
     }
 
-    public async Task<T?> DeleteByIdAsync(Guid id)
+    public async Task<T> DeleteByIdAsync(Guid id)
     {
         var entity = await _context.Set<T>().FindAsync(id);
 
         if (entity == null)
-            throw new KeyNotFoundException($"Entity with id {id} not found");
+            throw new NotFoundException(typeof(T).Name, id);
         
         _context.Remove(entity);
         await _context.SaveChangesAsync();
--- src/TodoApi/Repositories/IBaseRepository.cs
+++ src/TodoApi/Repositories/IBaseRepository.cs
@@ -3,9 +3,9 @@
 public interface IBaseRepository<T>
     where T: class
 {
-    Task<T?> AddAsync(T entity);
-    Task<T?> UpdateAsync(T entity);
-    Task<T?> GetByIdAsync(Guid id);
-    Task<T?> DeleteByIdAsync(Guid id);
+    Task<T> AddAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task<T> GetByIdAsync(Guid id);
+    Task<T> DeleteByIdAsync(Guid id);
     
 }
